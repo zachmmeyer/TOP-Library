a127e39846fdebc9b858b1172ce0afd74ee08fa7
Zach Meyer <34407561+zachmmeyer@users.noreply.github.com>
2021-01-27 18:55:01 -0500

    Add form for adding books to library

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read = false) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages, read: ${this.read ? 'yes' : 'no'}`;
-}
+  return `${this.title} by ${this.author}, ${this.pages} pages, read: ${
+    this.read ? "yes" : "no"
+  }`;
+};
 
-const sorcerorsStone = new Book("Harry Potter and The Sorceror's Stone", "J.K. Rowling", 223, false);
+const sorcerorsStone = new Book(
+  "Harry Potter and The Sorceror's Stone",
+  "J.K. Rowling",
+  223,
+  false
+);
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 310, true);
 
-console.log(sorcerorsStone.info());
-console.log(theHobbit.info());
+//console.log(sorcerorsStone.info());
+// console.log(theHobbit.info());
+
+myLibrary.push(theHobbit);
+myLibrary.push(sorcerorsStone);
+
+console.log(myLibrary);
