aca107ea146a6df0373bac007e9a9d7ad2e642c3
Zach Meyer <34407561+zachmmeyer@users.noreply.github.com>
2021-01-27 22:14:03 -0500

    Format with Prettier

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let myLibrary;
 
-if (!localStorage.getItem('myLibrary')) {
+if (!localStorage.getItem("myLibrary")) {
   myLibrary = new Array();
   console.log("Initializing new library");
 } else {
-  const tempLibrary = localStorage.getItem('myLibrary');
+  const tempLibrary = localStorage.getItem("myLibrary");
   myLibrary = JSON.parse(tempLibrary);
   console.log("Restoring library from storage");
 }
@@ -22,7 +22,7 @@ Book.prototype.info = function () {
   }`;
 };
 
-function addBookToLibrary () {
+function addBookToLibrary() {
   const formTitle = document.forms["add-book-form"]["title"].value;
   const formAuthor = document.forms["add-book-form"]["author"].value;
   const formPages = document.forms["add-book-form"]["pages"].value;
@@ -31,15 +31,15 @@ function addBookToLibrary () {
   if (document.forms["add-book-form"]["clearStorage"].checked) {
     localStorage.clear();
   } else if (formTitle == "" || formAuthor == "" || formPages == "") {
-      //Does not submit book missing information to library
+    //Does not submit book missing information to library
   } else {
     const formTitle = document.forms["add-book-form"]["title"].value;
     const formAuthor = document.forms["add-book-form"]["author"].value;
     const formPages = document.forms["add-book-form"]["pages"].value;
     const formRead = document.forms["add-book-form"]["read"].value;
-    const book = new Book(formTitle,formAuthor,formPages,formRead)
+    const book = new Book(formTitle, formAuthor, formPages, formRead);
     myLibrary.push(book);
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
   }
 }
 
